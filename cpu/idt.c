2bf1b833407b92adc03f8f199f710a2f2703eab5
Tarithj <tarithj@gmail.com>
2021-05-26 15:56:19 +0530

    change removed __ between asm and volatile

--- cpu/idt.c
+++ cpu/idt.c
@@ -14,7 +14,7 @@ void set_idt()
     idt_reg.base = (u32)&idt;
     idt_reg.limit = IDT_ENTRIES * sizeof(idt_gate_t) - 1;
     /* Don't make the mistake of loading &idt -- always load &idt_reg */
-    __asm__ __volatile__("lidtl (%0)"
+    asm volatile("lidtl (%0)"
                          :
                          : "r"(&idt_reg));
 }
