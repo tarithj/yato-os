0b783e5fbcfdb337b8328c15e905714ad939f808
Tarith <tarithj@gmail.com>
2021-06-24 09:29:29 +0530

    added char limit

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -5,6 +5,7 @@
 #include "../libc/string.h"
 #include "../libc/function.h"
 #include "../kernel/kernel.h"
+#include "../drivers/sound.h"
 #include <stdint.h>
 
 #define BACKSPACE 0x0E
@@ -13,40 +14,68 @@
 static char key_buffer[256];
 
 #define SC_MAX 57
-const char *sc_name[] = { "ERROR", "Esc", "1", "2", "3", "4", "5", "6", 
-    "7", "8", "9", "0", "-", "=", "Backspace", "Tab", "Q", "W", "E", 
-        "R", "T", "Y", "U", "I", "O", "P", "[", "]", "Enter", "Lctrl", 
-        "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "`", 
-        "LShift", "\\", "Z", "X", "C", "V", "B", "N", "M", ",", ".", 
-        "/", "RShift", "Keypad *", "LAlt", "Spacebar"};
-const char sc_ascii[] = { '?', '?', '1', '2', '3', '4', '5', '6',     
-    '7', '8', '9', '0', '-', '=', '?', '?', 'Q', 'W', 'E', 'R', 'T', 'Y', 
-        'U', 'I', 'O', 'P', '[', ']', '?', '?', 'A', 'S', 'D', 'F', 'G', 
-        'H', 'J', 'K', 'L', ';', '\'', '`', '?', '\\', 'Z', 'X', 'C', 'V', 
-        'B', 'N', 'M', ',', '.', '/', '?', '?', '?', ' '};
+const char *sc_name[] = {"ERROR", "Esc", "1", "2", "3", "4", "5", "6",
+                         "7", "8", "9", "0", "-", "=", "Backspace", "Tab", "Q", "W", "E",
+                         "R", "T", "Y", "U", "I", "O", "P", "[", "]", "Enter", "Lctrl",
+                         "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "`",
+                         "LShift", "\\", "Z", "X", "C", "V", "B", "N", "M", ",", ".",
+                         "/", "RShift", "Keypad *", "LAlt", "Spacebar"};
+const char sc_ascii[] = {'?', '?', '1', '2', '3', '4', '5', '6',
+                         '7', '8', '9', '0', '-', '=', '?', '?', 'Q', 'W', 'E', 'R', 'T', 'Y',
+                         'U', 'I', 'O', 'P', '[', ']', '?', '?', 'A', 'S', 'D', 'F', 'G',
+                         'H', 'J', 'K', 'L', ';', '\'', '`', '?', '\\', 'Z', 'X', 'C', 'V',
+                         'B', 'N', 'M', ',', '.', '/', '?', '?', '?', ' '};
 
-static void keyboard_callback(registers_t *regs) {
+static void keyboard_callback(registers_t *regs)
+{
     /* The PIC leaves us the scancode in port 0x60 */
     uint8_t scancode = port_byte_in(0x60);
-    
-    if (scancode > SC_MAX) return;
-    if (scancode == BACKSPACE) {
-        backspace(key_buffer);
-        kprint_backspace();
-    } else if (scancode == ENTER) {
+
+    if (scancode > SC_MAX)
+        return;
+    if (scancode == BACKSPACE)
+    {
+        int offset = get_cursor_offset();
+        int row = get_offset_col(offset);
+
+        if (row == 0)
+        {
+            beep();
+        }
+        else
+        {
+            backspace(key_buffer);
+            kprint_backspace();
+        }
+    }
+    else if (scancode == ENTER)
+    {
         kprint("\n");
         user_input(key_buffer); /* kernel-controlled function */
         key_buffer[0] = '\0';
-    } else {
-        char letter = sc_ascii[(int)scancode];
-        /* Remember that kprint only accepts char[] */
-        char str[2] = {letter, '\0'};
-        append(key_buffer, letter);
-        kprint(str);
+    }
+    else
+    {
+        int offset = get_cursor_offset();
+        int col = get_offset_col(offset);
+
+        if (col + 1 == MAX_COLS)
+        {
+            beep();
+        }
+        else
+        {
+            char letter = sc_ascii[(int)scancode];
+            /* Remember that kprint only accepts char[] */
+            char str[2] = {letter, '\0'};
+            append(key_buffer, letter);
+            kprint(str);
+        }
     }
     UNUSED(regs);
 }
 
-void init_keyboard() {
-   register_interrupt_handler(IRQ1, keyboard_callback); 
+void init_keyboard()
+{
+    register_interrupt_handler(IRQ1, keyboard_callback);
 }
